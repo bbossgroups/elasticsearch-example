d216a0b78b20dcc521c55ee70bce093ec12e02af
yinbp <yin-bp@163.com>
2018-06-26 20:43:20 +0800

    更新注释

--- src/main/java/org/bboss/elasticsearchtest/script/ScriptImpl.java
+++ src/main/java/org/bboss/elasticsearchtest/script/ScriptImpl.java
@@ -25,16 +25,19 @@ public class ScriptImpl {
 	private String mappath = "esmapper/demo.xml";
 
 	public void updateDocumentByScriptPath(){
+		//初始化数据，会创建type为demo的indice demo，并添加docid为2的文档
 		DocumentCRUD documentCRUD = new DocumentCRUD();
 		documentCRUD.testCreateIndice();
 		documentCRUD.testBulkAddDocument();
 		//创建加载配置文件的客户端工具，用来检索文档，单实例多线程安全
 		ClientInterface clientUtil = ElasticSearchHelper.getConfigRestClientUtil(mappath);
 		Map<String,Object> params = new HashMap<String,Object>();
-		//设置applicationName1和applicationName2两个变量的值
+		//为id为2的文档增加last和nick两个属性
 		params.put("last","gaudreau");
 		params.put("nick","hockey");
+		//通过script脚本为id为2的文档增加last和nick两个属性，为了演示效果强制refresh，实际环境慎用
 		clientUtil.updateByPath("demo/demo/2/_update?refresh","scriptDsl",params);
+		//获取更新后的文档，会看到新加的2个字段属性
 		String doc = clientUtil.getDocument("demo","demo","2");
 		System.out.println(doc);
 
