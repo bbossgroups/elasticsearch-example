79817d53debcd3a412017a8a4b31e7bc09ff0f5f
yinbp <yin-bp@163.com>
2018-09-04 23:41:32 +0800

    完善测试代码

--- src/test/java/org/bboss/elasticsearchtest/scroll/TestScrollAPIQuery.java
+++ src/test/java/org/bboss/elasticsearchtest/scroll/TestScrollAPIQuery.java
@@ -33,7 +33,8 @@ public class TestScrollAPIQuery {
 		List<String > scrollIds = new ArrayList<String>();
 		long starttime = System.currentTimeMillis();
 		Map params = new HashMap();
-		params.put("size", 10000);//每页100条记录
+		params.put("size", 10000);//每页10000条记录
+		//scroll上下文有效期1分钟
 		ESDatas<Map> response = clientUtil.scroll("demo/_search","scrollQuery","1m",params,Map.class);
 		List<Map> datas = response.getDatas();
 		long realTotalSize = datas.size();
@@ -57,8 +58,10 @@ public class TestScrollAPIQuery {
 		Map params = new HashMap();
 		params.put("sliceMax", max);//最多6个slice，不能大于share数
 		params.put("size", 100);//每页100条记录
+		//scroll上下文有效期1分钟
 		ESDatas<Map> sliceResponse = clientUtil.scrollSlice("demo/_search",
-				"scrollSliceQuery", params,"1m",Map.class,false);
+				"scrollSliceQuery", params,"1m",Map.class,
+				false);//false表示串行
 		System.out.println("totalSize:"+sliceResponse.getTotalSize());
 		System.out.println("realSize size:"+sliceResponse.getDatas().size());
 	}
@@ -77,8 +80,10 @@ public class TestScrollAPIQuery {
 		Map params = new HashMap();
 		params.put("sliceMax", max);//最多6个slice，不能大于share数，必须使用sliceMax作为变量名称
 		params.put("size", 100);//每页100条记录
+		//scroll上下文有效期2分钟
 		ESDatas<Map> sliceResponse = clientUtil.scrollSlice("demo/_search",
-				"scrollSliceQuery", params,"2m",Map.class,true);
+				"scrollSliceQuery", params,"2m",Map.class,
+				true);//true表示并行
 		System.out.println("totalSize:"+sliceResponse.getTotalSize());
 		System.out.println("realSize size:"+sliceResponse.getDatas().size());
 
@@ -94,6 +99,7 @@ public class TestScrollAPIQuery {
 		Map params = new HashMap();
 		params.put("size", 5000);//每页5000条记录
 		//采用自定义handler函数处理每个scroll的结果集后，response中只会包含总记录数，不会包含记录集合
+		//scroll上下文有效期1分钟
 		ESDatas<Map> response = clientUtil.scroll("demo/_search", "scrollQuery", "1m", params, Map.class, new ScrollHandler<Map>() {
 			public void handle(ESDatas<Map> response) throws Exception {//自己处理每次scroll的结果
 				List<Map> datas = response.getDatas();
@@ -121,6 +127,7 @@ public class TestScrollAPIQuery {
 		params.put("sliceMax", max);//最多6个slice，不能大于share数，必须使用sliceMax作为变量名称
 		params.put("size", 1000);//每页1000条记录
 		//采用自定义handler函数处理每个slice scroll的结果集后，sliceResponse中只会包含总记录数，不会包含记录集合
+		//scroll上下文有效期1分钟
 		ESDatas<Map> sliceResponse = clientUtil.scrollSlice("demo/_search",
 				"scrollSliceQuery", params,"1m",Map.class, new ScrollHandler<Map>() {
 					public void handle(ESDatas<Map> response) throws Exception {//自己处理每次scroll的结果
@@ -128,7 +135,8 @@ public class TestScrollAPIQuery {
 						long totalSize = response.getTotalSize();
 						System.out.println("totalSize:"+totalSize+",datas.size:"+datas.size());
 					}
-				},false);
+				},
+				false);//false表示串行
 		long totalSize = sliceResponse.getTotalSize();
 
 		System.out.println("totalSize:"+totalSize);
@@ -149,6 +157,7 @@ public class TestScrollAPIQuery {
 		params.put("sliceMax", max);//最多6个slice，不能大于share数，必须使用sliceMax作为变量名称
 		params.put("size", 1000);//每页1000条记录
 		//采用自定义handler函数处理每个slice scroll的结果集后，sliceResponse中只会包含总记录数，不会包含记录集合
+		//scroll上下文有效期1分钟
 		ESDatas<Map> sliceResponse = clientUtil.scrollSlice("demo/_search",
 				"scrollSliceQuery", params,"1m",Map.class, new ScrollHandler<Map>() {
 					public void handle(ESDatas<Map> response) throws Exception {//自己处理每次scroll的结果,注意结果是异步检索的
@@ -156,7 +165,8 @@ public class TestScrollAPIQuery {
 						long totalSize = response.getTotalSize();
 						System.out.println("totalSize:"+totalSize+",datas.size:"+datas.size());
 					}
-				},true);
+				},
+				true);//true表示并行
 
 		long totalSize = sliceResponse.getTotalSize();
 		System.out.println("totalSize:"+totalSize);
