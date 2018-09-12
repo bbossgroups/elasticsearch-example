2bdf89031c0563f1eba7ec1d359b7b2501806a6d
yinbp <yin-bp@163.com>
2018-09-12 11:03:37 +0800

    定时任务导入功能优化
    定时任务导入功能优化

--- src/test/java/org/bboss/elasticsearchtest/db2es/ScheduleImportTaskTest.java
+++ src/test/java/org/bboss/elasticsearchtest/db2es/ScheduleImportTaskTest.java
@@ -50,7 +50,7 @@ public class ScheduleImportTaskTest {
 
 
 		//指定导入数据的sql语句，必填项，可以设置自己的提取逻辑
-		importBuilder.setSql("select * from td_sm_log");
+		importBuilder.setSql("select * from td_sm_log where log_id > #[logId]");
 		/**
 		 * es相关配置
 		 */
@@ -69,6 +69,7 @@ public class ScheduleImportTaskTest {
 		importBuilder.setFromFirst(true);
 		importBuilder.setLastValueStorePath("testdb");
 		importBuilder.setLastValueStoreTableName("logs");
+		importBuilder.setLastValueType(0);
 
 //		importBuilder.
 
@@ -81,7 +82,7 @@ public class ScheduleImportTaskTest {
 		importBuilder.setContinueOnError(true);//任务出现异常，是否继续执行作业：true（默认值）继续执行 false 中断作业执行
 		importBuilder.setAsyn(false);//true 异步方式执行，不等待所有导入作业任务结束，方法快速返回；false（默认值） 同步方式执行，等待所有导入作业任务结束，所有作业结束后方法才返回
 		importBuilder.setEsIdField("log_id");
-		importBuilder.setDebugResponse(false);//设置是否将每次处理的reponse打印到日志文件中，默认false，不打印响应报文将大大提升性能，只有在需要的时候才，log日志级别同时要设置为INFO
+		importBuilder.setDebugResponse(true);//设置是否将每次处理的reponse打印到日志文件中，默认false，不打印响应报文将大大提升性能，只有在需要的时候才，log日志级别同时要设置为INFO
 //		importBuilder.setDiscardBulkResponse(true);//设置是否需要批量处理的响应报文，不需要设置为false，true为需要，默认true，如果不需要响应报文将大大提升处理速度
 		/**
 		 * 执行数据库表数据导入es操作
@@ -91,7 +92,7 @@ public class ScheduleImportTaskTest {
 
 		long count = ElasticSearchHelper.getRestClientUtil().countAll("dbdemo");
 		try {
-			Long dbcount = SQLExecutor.queryObject(Long.class, "select count(1) from (" + importBuilder.getSql()+ ") b");
+			Long dbcount = SQLExecutor.queryObjectWithDBName(Long.class, importBuilder.getDbName(),"select count(1) from (select * from td_sm_log) b");
 			System.out.println("数据库记录数dbcount:" + dbcount);
 		}
 		catch (Exception e){
