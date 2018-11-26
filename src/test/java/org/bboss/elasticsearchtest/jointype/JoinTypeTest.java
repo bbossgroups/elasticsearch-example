d4fc68d1577deebeb71c66421be5a827bf0ff279
yinbp <yin-bp@163.com>
2018-11-26 18:40:18 +0800

    bug修复：通过bboss直接跑这种脚本，会漏掉最后一条记录的bug,通过在dsl配置上面加上trim="false"来控制不要去掉bulk dsl后面的换行符号，使用示例：  <!--     导入公司信息：     -->     <property name="bulkImportCompanyData"  trim="false">         <![CDATA[             { "index": { "_id": "london" }}             { "name": "London Westminster", "city": "London", "country": "UK" ,"companyId":"london"}             { "index": { "_id": "liverpool" }}             { "name": "Liverpool Central", "city": "Liverpool", "country": "UK" ,"companyId":"liverpool"}             { "index": { "_id": "paris" }}             { "name": "Champs Élysées", "city": "Paris", "country": "France","companyId":"paris" }         ]]>     </property>     <!--    导入雇员信息：    -->     <property name="bulkImportEmployeeData" trim="false">         <![CDATA[             { "index": { "_id": 1, "parent": "london" }}             { "name": "Alice Smith", "birthday": "1970-10-24", "hobby": "hiking" ,"companyId":"london","employeeId":1 }             { "index": { "_id": 2, "parent": "london" }}             { "name": "Mark Thomas", "birthday": "1982-05-16", "hobby": "diving" ,"companyId":"london","employeeId":2}             { "index": { "_id": 3, "parent": "liverpool" }}             { "name": "Barry Smith", "birthday": "1979-04-01", "hobby": "hiking" ,"companyId":"liverpool","employeeId":3}             { "index": { "_id": 4, "parent": "paris" }}             { "name": "Adrien Grand", "birthday": "1987-05-11", "hobby": "horses" ,"companyId":"paris","employeeId":4}             { "index": { "_id": 5, "parent": "paris" }}             { "name": "Adrien Green", "birthday": "1977-05-12", "hobby": "dancing" ,"companyId":"paris","employeeId":5}         ]]>     </property>
    功能改进：支持多个sontype父子子查询的orm功能，只用示例：
    
    public void hasChildSearchReturnParent2ndChildren(){
    		ClientInterface clientUtil = ElasticSearchHelper.getConfigRestClientUtil("esmapper/indexparentchild.xml");
    		Map<String,Object> params = new HashMap<String,Object>();
    		params.put("name","Alice Smith");
    
    		try {
    			ESInnerHitSerialThreadLocal.setESInnerTypeReferences("employee",Employee.class);//指定inner查询结果对于雇员类型和对应的orm对象类
                			ESInnerHitSerialThreadLocal.setESInnerTypeReferences("othersontype",Otherson.class);//指定inner查询结果对于其他子文档类型名称和orm对象类
    			ESDatas<Company> escompanys = clientUtil.searchList("company/company/_search",
    													"hasChildSearchReturnParent2ndChildren",params,Company.class);
    			long totalSize = escompanys.getTotalSize();
    			List<Company> companyList = escompanys.getDatas();//获取符合条件的公司
    			//查看公司下面的雇员信息（符合检索条件的雇员信息）
    			for (int i = 0; i < companyList.size(); i++) {
    				Company company = companyList.get(i);
    				List<Employee> employees = ResultUtil.getInnerHits(company.getInnerHits(), "employee");
    				System.out.println(employees.size());
    				List<OtherSon> otherSons = ResultUtil.getInnerHits(company.getInnerHits(), "othersontype");
    				System.out.println(otherSons.size());
    			}
    		}
    		finally{
    			ESInnerHitSerialThreadLocal.clean();//清空inner查询结果对于雇员类型
    		}
    	}

--- src/test/java/org/bboss/elasticsearchtest/jointype/JoinTypeTest.java
+++ src/test/java/org/bboss/elasticsearchtest/jointype/JoinTypeTest.java
@@ -32,26 +32,28 @@ import java.util.Map;
  * <p>Description: </p>
  * <p></p>
  * <p>Copyright (c) 2018</p>
- * @Date 2018/9/11 11:47
+ *
  * @author biaoping.yin
  * @version 1.0
+ * @Date 2018/9/11 11:47
  */
 public class JoinTypeTest {
 	private ClientInterface clientInterface = ElasticSearchHelper.getConfigRestClientUtil("esmapper/joinparentchild.xml");
-	private void createPagerIndice(){
+
+	private void createPagerIndice() {
 		try {
 			clientInterface.dropIndice("pager");
-		}
-		catch (Exception e){
+		} catch (Exception e) {
 
 		}
-		String response = clientInterface.createIndiceMapping("pager","createPagerIndice");
+		String response = clientInterface.createIndiceMapping("pager", "createPagerIndice");
 		System.out.println(response);
 		String mapping = clientInterface.getIndexMapping("pager");
 		System.out.println(mapping);
 
 	}
-	private void importDatas(){
+
+	private void importDatas() {
 
 
 		ClientInterface restClient = ElasticSearchHelper.getRestClientUtil();
@@ -60,21 +62,21 @@ public class JoinTypeTest {
 		StringBuilder data = new StringBuilder();
 		data.append(esInfo.getTemplate().trim());
 		data.append("\n");
-		restClient.executeHttp("pager/pagertype/_bulk?refresh",data.toString(), ClientUtil.HTTP_POST);
+		restClient.executeHttp("pager/pagertype/_bulk?refresh", data.toString(), ClientUtil.HTTP_POST);
 		//导入数据,并且实时刷新，测试需要，实际环境不要带refresh
 		data.setLength(0);
 		esInfo = clientInterface.getESInfo("bulkImportAnswerData");
 		data = new StringBuilder();
 		data.append(esInfo.getTemplate().trim());
 		data.append("\n");
-		restClient.executeHttp("pager/pagertype/_bulk?refresh",data.toString(),ClientUtil.HTTP_POST);
+		restClient.executeHttp("pager/pagertype/_bulk?refresh", data.toString(), ClientUtil.HTTP_POST);
 		//导入数据,并且实时刷新，测试需要，实际环境不要带refresh
 		data.setLength(0);
 		esInfo = clientInterface.getESInfo("bulkImportCommentData");
 		data = new StringBuilder();
 		data.append(esInfo.getTemplate().trim());
 		data.append("\n");
-		restClient.executeHttp("pager/pagertype/_bulk?refresh",data.toString(),ClientUtil.HTTP_POST);
+		restClient.executeHttp("pager/pagertype/_bulk?refresh", data.toString(), ClientUtil.HTTP_POST);
 		long companycount = clientInterface.countAll("pager/pagertype");
 		System.out.println(companycount);
 
@@ -83,17 +85,17 @@ public class JoinTypeTest {
 	/**
 	 * 根据问题名称检索答案和评论信息，同时返回问题及答案和评论信息
 	 */
-	public void hasParentSearchByCountryReturnParent2ndMultiChildren(){
-		Map<String,Object> params = new HashMap<String,Object>();//没有检索条件，构造一个空的参数对象
+	public void hasParentSearchByCountryReturnParent2ndMultiChildren() {
+		Map<String, Object> params = new HashMap<String, Object>();//没有检索条件，构造一个空的参数对象
 //		params.put("name","Alice Smith");
 
 		try {
 			//设置子文档的类型和对象映射关系
-			ESInnerHitSerialThreadLocal.setESInnerTypeReferences("answer", Answer.class);//指定inner查询结果对于exam类型和对应的对象类型Exam
-			ESInnerHitSerialThreadLocal.setESInnerTypeReferences("comment", Comment.class);//指定inner查询结果对于diagnosis类型和对应的对象类型Diagnosis
-			String response = clientInterface.executeRequest("pager/pagertype/_search","hasParentSearchByNameReturnAnswerAndComment",params);
+			ESInnerHitSerialThreadLocal.setESInnerTypeReferences("answer", Answer.class);//指定inner查询结果对于answer类型和对应的对象类型Answer
+			ESInnerHitSerialThreadLocal.setESInnerTypeReferences("comment", Comment.class);//指定inner查询结果对于comment类型和对应的对象类型Comment
+			String response = clientInterface.executeRequest("pager/pagertype/_search", "hasParentSearchByNameReturnAnswerAndComment", params);
 			ESDatas<Question> escompanys = clientInterface.searchList("pager/pagertype/_search",
-					"hasParentSearchByNameReturnAnswerAndComment",params,Question.class);
+					"hasParentSearchByNameReturnAnswerAndComment", params, Question.class);
 //			escompanys = clientUtil.searchAll("client_info",Basic.class);
 			long totalSize = escompanys.getTotalSize();
 			List<Question> clientInfos = escompanys.getDatas();//获取符合条件的数据
@@ -101,41 +103,40 @@ public class JoinTypeTest {
 			for (int i = 0; clientInfos != null && i < clientInfos.size(); i++) {
 				Question question = clientInfos.get(i);
 				List<Answer> answers = ResultUtil.getInnerHits(question.getInnerHits(), "answer");
-				if(answers != null)
+				if (answers != null)
 					System.out.println(answers.size());
 				List<Comment> comments = ResultUtil.getInnerHits(question.getInnerHits(), "comment");
-				if(comments != null)
+				if (comments != null)
 					System.out.println(comments.size());
 
 
 			}
-		}
-		finally{
+		} finally {
 			ESInnerHitSerialThreadLocal.clean();//清空inner查询结果对于雇员类型
 		}
 	}
+
 	/**
 	 * 根据问题名称，检索答案和评论信息以及对应的问题信息
 	 */
-	public void hasParentSearchByCountryReturnParent2ndChildren(){
+	public void hasParentSearchByCountryReturnParent2ndChildren() {
 
-		Map<String,Object> params = new HashMap<String,Object>();
-		params.put("name","q4");
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("name", "q4");
 
 		try {
-			ESInnerHitSerialThreadLocal.setESInnerTypeReferences(Question.class);//指定inner查询结果对于公司类型,公司只有一个文档类型，索引不需要显示指定company类型信息
+			ESInnerHitSerialThreadLocal.setESInnerTypeReferences(Question.class);//指定inner查询结果对于问题类型,问题只有一个文档类型，索引不需要显示指定Question类型信息
 			ESDatas<Answer> escompanys = clientInterface.searchList("pager/pagertype/_search",
-					"hasParentSearchByQuestionNameReturnParent2ndChildren",params,Answer.class);
+					"hasParentSearchByQuestionNameReturnParent2ndChildren", params, Answer.class);
 			List<Answer> employeeList = escompanys.getDatas();//获取符合条件的雇员数据
 			long totalSize = escompanys.getTotalSize();
 			//查看每个雇员对应的公司信息
-			for(int i = 0;  i < employeeList.size(); i ++) {
+			for (int i = 0; i < employeeList.size(); i++) {
 				Answer employee = employeeList.get(i);
 				List<Question> companies = ResultUtil.getInnerHits(employee.getInnerHits(), "question");
 				System.out.println(companies.size());
 			}
-		}
-		finally{
+		} finally {
 			ESInnerHitSerialThreadLocal.clean();//清空inner查询结果对于公司类型
 		}
 	}
@@ -144,51 +145,48 @@ public class JoinTypeTest {
 	 * 根据问题名称，检索答案信息
 	 */
 
-	public void hasParentSearchByName(){
+	public void hasParentSearchByName() {
 
-		Map<String,Object> params = new HashMap<String,Object>();
-		params.put("name","q4");
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("name", "q4");
 
-		try {
-			ESDatas<Answer> escompanys = clientInterface.searchList("pager/pagertype/_search",
-					"hasParentSearchByName",params,Answer.class);
-			List<Answer> employeeList = escompanys.getDatas();//获取符合条件的雇员数据
-			long totalSize = escompanys.getTotalSize();
-			//查看每个雇员对应的公司信息
-			for(int i = 0;  i < employeeList.size(); i ++) {
-				Answer employee = employeeList.get(i);
-				System.out.println(employee.getDatatype());
-			}
+
+		ESDatas<Answer> escompanys = clientInterface.searchList("pager/pagertype/_search",
+				"hasParentSearchByName", params, Answer.class);
+		List<Answer> employeeList = escompanys.getDatas();//获取符合条件的雇员数据
+		long totalSize = escompanys.getTotalSize();
+		//查看每个雇员对应的公司信息
+		for (int i = 0; i < employeeList.size(); i++) {
+			Answer employee = employeeList.get(i);
+			System.out.println(employee.getDatatype());
 		}
-		finally{
-			ESInnerHitSerialThreadLocal.clean();//清空inner查询结果对于公司类型
-		}
+
+
 	}
 
 	/**
 	 * 根据问题id，检索答案信息
 	 */
-	public void hasParentIdSearch(){
-		Map<String,Object> params = new HashMap<String,Object>();
-		params.put("id","1");
+	public void hasParentIdSearch() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("id", "1");
 
-		try {
-			ESDatas<Answer> escompanys = clientInterface.searchList("pager/pagertype/_search",
-					"hasParentIdSearch",params,Answer.class);
-			List<Answer> employeeList = escompanys.getDatas();//获取符合条件的雇员数据
-			long totalSize = escompanys.getTotalSize();
-			//查看每个雇员对应的公司信息
-			for(int i = 0;  i < employeeList.size(); i ++) {
-				Answer employee = employeeList.get(i);
-				System.out.println(employee.getDatatype());
-			}
+
+		ESDatas<Answer> escompanys = clientInterface.searchList("pager/pagertype/_search",
+				"hasParentIdSearch", params, Answer.class);
+		List<Answer> employeeList = escompanys.getDatas();//获取符合条件的雇员数据
+		long totalSize = escompanys.getTotalSize();
+		//查看每个雇员对应的公司信息
+		for (int i = 0; i < employeeList.size(); i++) {
+			Answer employee = employeeList.get(i);
+			System.out.println(employee.getDatatype());
 		}
-		finally{
-			ESInnerHitSerialThreadLocal.clean();//清空inner查询结果对于公司类型
-		}
+
+
 	}
+
 	@Test
-	public void testJoin(){
+	public void testJoin() {
 		createPagerIndice();
 		importDatas();
 		hasParentIdSearch();
