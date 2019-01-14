f11d2b3fb6a08a22dcf6c0ecb35f57cfc0fa5dae
yinbp <yin-bp@163.com>
2019-01-14 23:11:28 +0800

    1.增加一组简单的聚合统计api 2.完善slice并行多线程inner_hit查询时orm class type异步绑定机制，

---
+++ src/test/java/org/bboss/elasticsearchtest/aggs/TestTermAgg.java
@@ -0,0 +1,47 @@
+package org.bboss.elasticsearchtest.aggs;
+/**
+ * Copyright 2008 biaoping.yin
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.frameworkset.elasticsearch.ElasticSearchHelper;
+import org.frameworkset.elasticsearch.client.ClientInterface;
+import org.frameworkset.elasticsearch.entity.ESAggDatas;
+import org.frameworkset.elasticsearch.entity.LongAggHit;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>Description: </p>
+ * <p></p>
+ * <p>Copyright (c) 2018</p>
+ * @Date 2019/1/14 16:13
+ * @author biaoping.yin
+ * @version 1.0
+ */
+public class TestTermAgg {
+	ClientInterface clientInterface = ElasticSearchHelper.getConfigRestClientUtil("esmapper/testagg.xml");
+	@Test
+	public void termAgg(){
+
+		Map params = null;//聚合统计条件参数
+		//一行代码，执行每个服务的访问量总数统计
+		ESAggDatas<LongAggHit> response = clientInterface.searchAgg("trace-*/_search","termAgg",params,LongAggHit.class,"traces");
+		List<LongAggHit> aggHitList = response.getAggDatas();//每个服务的访问量
+		long totalSize = response.getTotalSize();//总访问量
+
+	}
+}
