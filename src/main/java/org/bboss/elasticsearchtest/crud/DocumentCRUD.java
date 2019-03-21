16bfe3c523e345fa4561b067f15ea5baa2ce44b6
yinbp <yin-bp@163.com>
2019-03-21 21:55:15 +0800

    完善文档

--- src/main/java/org/bboss/elasticsearchtest/crud/DocumentCRUD.java
+++ src/main/java/org/bboss/elasticsearchtest/crud/DocumentCRUD.java
@@ -555,6 +555,9 @@ public class DocumentCRUD {
 				"searchDatas",//esmapper/demo.xml中定义的dsl语句
 				params,//变量参数
 				Demo.class);//返回的文档封装对象类型
+
+		long count = clientUtil.count("demo/_search","searchDatas",//esmapper/demo.xml中定义的dsl语句
+				params);//变量参数
 		//获取结果对象列表，最多返回1000条记录
 		List<Demo> demos = esDatas.getDatas();
 
