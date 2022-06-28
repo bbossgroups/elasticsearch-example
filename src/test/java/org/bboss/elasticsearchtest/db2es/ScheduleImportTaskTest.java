b6ae3b85c7cd14653c584b07372da6f534f073a2
yinbp <yinbp@asiainfo.com>
2022-06-28 16:41:22 +0800

    数据同步架构重构：去掉所有源到目标builder，统一使用ImportBuilder构建器+InputConfig+OutputConfig来构建数据同步作业

--- src/test/java/org/bboss/elasticsearchtest/db2es/ScheduleImportTaskTest.java
+++ src/test/java/org/bboss/elasticsearchtest/db2es/ScheduleImportTaskTest.java
@@ -17,7 +17,9 @@ package org.bboss.elasticsearchtest.db2es;
 
 
 import org.frameworkset.tran.DataStream;
-import org.frameworkset.tran.db.input.es.DB2ESImportBuilder;
+import org.frameworkset.tran.config.ImportBuilder;
+import org.frameworkset.tran.plugin.db.input.DBInputConfig;
+import org.frameworkset.tran.plugin.es.output.ElasticsearchOutputConfig;
 import org.frameworkset.tran.schedule.ImportIncreamentConfig;
 import org.junit.Test;
 
@@ -38,7 +40,7 @@ public class ScheduleImportTaskTest {
 	 */
 	@Test
 	public void testSimpleLogDB2ESImportBuilderFromExternalDBConfig(){
-		DB2ESImportBuilder importBuilder = DB2ESImportBuilder.newInstance();
+		ImportBuilder importBuilder = ImportBuilder.newInstance();
 		//增量定时任务不要删表，但是可以通过删表来做初始化操作
 //		try {
 //			//清除测试表,导入的时候回重建表，测试的时候加上为了看测试效果，实际线上环境不要删表
@@ -54,22 +56,28 @@ public class ScheduleImportTaskTest {
 		// select * from td_sm_log where log_id > #[log_id] and parent_id = #[log_id]
 		// log_id和数据库对应的字段一致,就不需要设置setLastValueColumn信息，
 		// 但是需要设置setLastValueType告诉工具增量字段的类型
-
-		importBuilder.setSql("select * from td_sm_log where log_id > #[log_id]");
+		DBInputConfig dbInputConfig = new DBInputConfig();
+		dbInputConfig.setSql("select * from td_sm_log where log_id > #[log_id]");
+		importBuilder.setInputConfig(dbInputConfig);
 		/**
 		 * es相关配置
 		 */
-		importBuilder
+		ElasticsearchOutputConfig elasticsearchOutputConfig = new ElasticsearchOutputConfig();
+		elasticsearchOutputConfig
 				.setIndex("dbdemo") //必填项
-				.setIndexType("dbdemo") //必填项
+//				.setIndexType("dbdemo"); //必填项
 //				.setRefreshOption("refresh")//可选项，null表示不实时刷新，importBuilder.setRefreshOption("refresh");表示实时刷新
+				.setEsIdField("log_id")//设置文档主键，不设置，则自动产生文档id
+				.setDebugResponse(true);//设置是否将每次处理的reponse打印到日志文件中，默认false，不打印响应报文将大大提升性能，只有在需要的时候才，log日志级别同时要设置为INFO
+//				.setDiscardBulkResponse(true);//设置是否需要批量处理的响应报文，不需要设置为false，true为需要，默认true，如果不需要响应报文将大大提升处理速度
+		importBuilder.setOutputConfig(elasticsearchOutputConfig)
 				.setUseJavaName(true) //可选项,将数据库字段名称转换为java驼峰规范的名称，例如:doc_id -> docId
 				.setBatchSize(50);  //可选项,批量导入es的记录数，默认为-1，逐条处理，> 0时批量处理
 		//定时任务配置，
 		importBuilder.setFixedRate(false)//参考jdk timer task文档对fixedRate的说明
 //					 .setScheduleDate(date) //指定任务开始执行时间：日期
-				     .setDeyLay(1000L) // 任务延迟执行deylay毫秒后执行
-					 .setPeriod(10000L); //每隔period毫秒执行，如果不设置，只执行一次
+				.setDeyLay(1000L) // 任务延迟执行deylay毫秒后执行
+				.setPeriod(10000L); //每隔period毫秒执行，如果不设置，只执行一次
 //		importBuilder.setLastValueColumn("log_id");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
 		importBuilder.setFromFirst(true);//任务重启时，重新开始采集数据，适合于每次全量导入数据的情况，如果是全量导入，可以先删除原来的索引数据
 		importBuilder.setLastValueStorePath("testdb");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点
@@ -80,27 +88,26 @@ public class ScheduleImportTaskTest {
 //		importBuilder.
 
 		/**
-		 * 内置线程池配置，实现多线程并行数据导入功能，作业完成退出时，自动关闭线程池
+		 * 内置线程池配置，实现多线程并行数据导入功能，作业完成退出时自动关闭该线程池
 		 */
 		importBuilder.setParallel(true);//设置为多线程并行批量导入
 		importBuilder.setQueue(10);//设置批量导入线程池等待队列长度
 		importBuilder.setThreadCount(50);//设置批量导入线程池工作线程数量
 		importBuilder.setContinueOnError(true);//任务出现异常，是否继续执行作业：true（默认值）继续执行 false 中断作业执行
 		importBuilder.setAsyn(false);//true 异步方式执行，不等待所有导入作业任务结束，方法快速返回；false（默认值） 同步方式执行，等待所有导入作业任务结束，所有作业结束后方法才返回
-		importBuilder.setEsIdField("log_id");//设置文档主键，不设置，则自动产生文档id
-		importBuilder.setDebugResponse(true);//设置是否将每次处理的reponse打印到日志文件中，默认false，不打印响应报文将大大提升性能，只有在需要的时候才，log日志级别同时要设置为INFO
-//		importBuilder.setDiscardBulkResponse(true);//设置是否需要批量处理的响应报文，不需要设置为false，true为需要，默认true，如果不需要响应报文将大大提升处理速度
+
 		/**
 		 * 执行数据库表数据导入es操作
 		 */
 		DataStream dataStream = importBuilder.builder();
 		dataStream.execute();//执行导入操作
 
+		System.out.println();
 
-		
+
 	}
-	
-	
-	
+
+
+
 
 }
